02c9619c1fa91934d8dde971a9dd0be55a5dd9b1
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-08-23 23:04:53 +0530

    Time: 5 ms (21.76%), Space: 23.8 MB (9.69%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,41 +1,60 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        reverse(s.begin(),s.end());
+    string formatString(string & s){
         int n=s.size();
-        int i=0,j=0;
-        while(j<n){
-            if(s[j]==' '){
-                reverse(s.begin()+i,s.begin()+i+j-i);
-                i=j+1;
-                j++;
-            }
-            else{
-                j++;
-            }
-        }
-        reverse(s.begin()+i,s.begin()+i+j-i);
-        // now remove extra spaces
+        int i=0;
         string ans="";
-        i=0,j=n-1;
-        while(s[i]==' '){
+        string temp="";
+        // skip starting spaces
+        while(i<n && s[i]==' '){
             i++;
         }
-        while(s[j]==' '){
-            j--;
-        }
-        bool flag=false;
-        for(;i<=j;i++){
-            if(s[i]==' ' && flag==false){
-                flag=true;
-                ans+=s[i];
-            }
-            else if(s[i]!=' '){
-                ans+=s[i];
-                flag=false;
-            }
 
+
+        while(i<n){
+            if(s[i]==' '){
+                temp+=" ";
+                ans+=temp;
+                temp="";
+                while(s[i]==' ' && i<n){
+                    i++;
+                }
+
+            }
+            else{
+                temp+=s[i++];
+            }
         }
+        if(temp!="")ans+=temp;
+
         return ans;
+    }
+    string reverseWords(string s) {
+        s=formatString(s);
+
+        // now reverse the words
+        int last=0;
+        string ans="";
+        string temp="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                temp+= ans!=""?" ":"";
+                ans=temp+ans;
+                temp="";
+            }
+            else{
+                temp+=s[i];
+            }
+        }
+        if(temp!=""){
+            if(ans!=""){
+
+                temp+=' ';
+            }
+            ans=temp+ans;
+        }
+
+        return ans;
+       
     }
 };
