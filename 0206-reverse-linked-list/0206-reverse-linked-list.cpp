6f7da42a08895f5d73f65641b2a99635e971f537
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-08-30 21:27:09 +0530

    Time: 0 ms (100%), Space: 13.7 MB (10.25%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -15,22 +15,19 @@ public:
         if(curr->next==NULL){
             ans=curr;
             curr->next=prev;
-
             prev->next=NULL;
             return;
         }
 
-
-
         findReverse(curr,curr->next,ans);
         curr->next=prev;
-        if(prev!=NULL)prev->next=NULL;
+        prev->next=NULL;
 
     }
     ListNode* reverseList(ListNode* head) {
         if(head==NULL || head->next==NULL)return head;
         ListNode* ans=NULL;
-        findReverse(NULL,head,ans);
+        findReverse(head,head->next,ans);
         return ans;
     }
 };
