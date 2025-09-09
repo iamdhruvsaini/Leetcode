7167ad60e17556d09bb21944d9fc47d758a5c071
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-09-09 23:35:20 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.59%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -17,9 +17,23 @@ public:
         }
 
     }
+
+    int solveByTabulation(vector<int>&cost,int n){
+        vector<int>dp(n+1,-1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i=2;i<=n;i++){
+            int currentCost = (i == n) ? 0 : cost[i];
+            dp[i] = std::min(dp[i - 1], dp[i - 2]) + currentCost;
+        }
+        return dp[n];
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int>dp(n+1,-1);
-        return solve(cost,n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(cost,n,dp);
+
+        return solveByTabulation(cost,n);
     }
 };
