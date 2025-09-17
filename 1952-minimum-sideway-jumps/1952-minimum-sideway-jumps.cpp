74c6617f4be276bdd8dff43ce31955b3901fe44b
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-09-18 00:12:20 +0530

    Time: 368 ms (30.53%), Space: 324.5 MB (26.41%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -21,12 +21,45 @@ public:
 
         }
     }
+
+    int solveTabulation(vector<int>& obstacles){
+        int n=obstacles.size();
+        vector<vector<int>>dp(n+1,vector<int>(4,0));
+        dp[n-1][1]=0;
+        dp[n-1][2]=0;
+        dp[n-1][3]=0;
+
+        for(int index=n-2;index>=0;index--){
+           for(int j=1;j<=3;j++){
+            if(obstacles[index+1]!=j){
+                dp[index][j]=dp[index+1][j];
+            }
+            else{
+                int ans=INT_MAX;
+                for(int i=1;i<=3;i++){
+                    if(i!=j && obstacles[index]!=i){
+                        int temp=1+dp[index+1][i];
+                        ans=min(ans,temp);
+                    }
+                }
+                dp[index][j]=ans;
+            }
+           }
+        }
+
+        return min(dp[0][2],min(1 + dp[0][1], 1 + dp[0][3]));
+
+    }
+
+
     int minSideJumps(vector<int>& obstacles) {
 
-        int n=obstacles.size();
-        vector<vector<int>>dp(n,vector<int>(4,-1));
+    //     int n=obstacles.size();
+    //     vector<vector<int>>dp(n,vector<int>(4,-1));
 
-       return solve(obstacles,0,2,dp);
+    //    return solve(obstacles,0,2,dp);
+
+        return solveTabulation(obstacles);
         
     }
 };
