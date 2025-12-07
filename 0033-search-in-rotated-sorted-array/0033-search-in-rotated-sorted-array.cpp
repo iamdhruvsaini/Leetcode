ec072b76b2000ff883fc4e20f207990bc820cb76
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-12-08 00:01:34 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.3%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
+    int search(vector<int>&arr, int target) {
         int s=0,e=arr.size()-1;
+
         while(s<=e){
             int mid=s+(e-s)/2;
-
-            if(arr[mid]==target)return mid;
-
+            if(arr[mid]==target){
+                return mid;
+            }
             else if(arr[mid]>=arr[s]){
-                //left part is sorted
-                if(target>=arr[s] && target<=arr[mid]){
+                // left part is sorted
+                if(target<=arr[mid] && target >=arr[s]){
                     e=mid-1;
                 }
                 else{
                     s=mid+1;
                 }
-            }else{
-                //right part is sorted
-                 if(target<=arr[e] && target>=arr[mid]){
+            }
+            else {
+                // right part is sorted
+                if(target>=arr[mid] && target <=arr[e]){
                     s=mid+1;
                 }
                 else{
@@ -25,6 +27,7 @@ public:
                 }
             }
         }
+
         return -1;
     }
 };
