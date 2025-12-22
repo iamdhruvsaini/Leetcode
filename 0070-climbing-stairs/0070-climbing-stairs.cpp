7d45c797984fce1256ff795bb1d3a78887881eb7
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-12-22 22:19:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (16.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,9 +10,31 @@ public:
 
         return dp[n]=case1+case2;
     }
+
+    int tabulation(int n){
+        vector<int>dp(n+1,-1);
+        dp[0]=1;
+
+        for(int i=1;i<=n;i++){
+            int case1=0;
+            if(i-1 >=0){
+                case1=dp[i-1];
+            }
+            int case2=0;
+            if(i-2>=0){
+                case2=dp[i-2];
+            }
+
+            dp[i]=case1+case2;
+
+        }
+
+
+        return dp[n];
+    }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        return tabulation(n);
 
     }
 };
