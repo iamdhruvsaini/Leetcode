c823e40c7c3e06821453106b7457d94b96bacf2b
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2025-12-26 23:57:27 +0530

    Time: 25 ms (67.65%), Space: 27.6 MB (14.41%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,7 +6,7 @@ public:
         if(dp[n][m]!=-1)return dp[n][m];
 
         if(text1[n-1]==text2[m-1]){
-            return 1+f(text1,text2,n-1,m-1,dp);
+            return dp[n][m]=1+f(text1,text2,n-1,m-1,dp);
         }
         return dp[n][m]=max(f(text1,text2,n-1,m,dp), f(text1,text2,n,m-1,dp));
     }
@@ -17,7 +17,25 @@ public:
 
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
+        function<int()> fnc =[n,m,&dp,&text1,&text2](){
+            for(int i=0;i<=n;i++)dp[i][0]=0;
+            for(int i=0;i<=m;i++)dp[0][i]=0;
 
-        return f(text1,text2,n,m,dp);
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    if(text1[i-1]==text2[j-1]){
+                        dp[i][j]=1+dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+                    } 
+
+                }
+            }
+
+            return dp[n][m];
+        };
+
+        return fnc();
     }
 };
