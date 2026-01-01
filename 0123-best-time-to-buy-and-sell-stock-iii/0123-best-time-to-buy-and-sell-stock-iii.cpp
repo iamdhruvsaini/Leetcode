a08c15e9fed8178cc17ffb93b42376c19274e33e
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-01 23:48:21 +0530

    Time: 412 ms (31.35%), Space: 211.3 MB (40.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,7 +17,30 @@ public:
     int maxProfit(vector<int>& prices) {
         // 3d dp
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(prices,0,1,2,dp); 
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        auto fnc=[&](){
+            for(int index=n-1;index>=0;index--){
+                for(int buy=0;buy<=1;buy++){
+                    for(int cap=1;cap<=2;cap++){
+                        if(buy){
+                            // you can invest or can skip
+                            dp[index][buy][cap] = max(-prices[index]+dp[index+1][0][cap] , dp[index+1][1][cap]);
+                        }
+                        else{
+                            // you can sell or sell later
+                            dp[index][buy][cap] = max(prices[index]+dp[index+1][1][cap-1], dp[index+1][0][cap]);
+                        }
+
+                    }
+                }
+            }
+
+            return dp[0][1][2];
+
+        };
+
+        return fnc();
+        // return f(prices,0,1,2,dp);
+
     }
 };
