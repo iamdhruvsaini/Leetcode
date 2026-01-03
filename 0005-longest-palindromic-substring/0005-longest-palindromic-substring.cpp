b3475ff6bc1ab9d2d73270996ce34c13d5578520
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-04 01:52:08 +0530

    Time: 5 ms (90.26%), Space: 11.5 MB (51.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,59 +1,54 @@
 class Solution {
 public:
+    vector<int> p;
 
-string solve(string &s){
-  int n=s.size();
-  
-  int maxi=-1;
-  int start=-1;
-  
-  for(int i=0;i<n;i++){
-    
-    // even palindrome
-    int left=i;
-    int right=i+1;
-    int evenLen=-1;
-    
-    while(left>=0 && right<n && s[left]==s[right]){
-      evenLen=max(evenLen,right-left+1);
-      left--;
-      right++;
-      
+    void run_manacher(const string &s) {
+        int n = s.length();
+        p.assign(n, 1);
+
+        int l = 0, r = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i < r) {
+                int mirror = l + r - i;
+                p[i] = min(r - i, p[mirror]);
+            }
+
+            while (i + p[i] < n &&
+                   i - p[i] >= 0 &&
+                   s[i + p[i]] == s[i - p[i]]) {
+                p[i]++;
+            }
+
+            if (i + p[i] > r) {
+                l = i - p[i];
+                r = i + p[i];
+            }
+        }
     }
-    
-    if(evenLen>maxi){
-      maxi=evenLen;
-      start=left+1;
-    }
-    
-    // odd len palindrome
-    left=i-1;
-    right=i+1;
-    int oddLen=-1;
-    
-    while( left>=0 && right<n && s[left]==s[right]){
-      oddLen=max(oddLen,right-left+1);
-      left--;
-      right++;
-    }
-    
-    if(oddLen>maxi){
-      maxi=oddLen;
-      start=left+1;
-    }
-  }
-  
-  
-  
-  if(maxi==-1)return s.substr(0,1);
-  else{
-    return s.substr(start,maxi);
-  }  
-  
-}
 
     string longestPalindrome(string s) {
-        return solve(s);
-        
+        if (s.empty()) return "";
+
+        // Build transformed string
+        string t;
+        for (char c : s) {
+            t += '#';
+            t += c;
+        }
+        t += '#';
+
+        run_manacher(t);
+
+        int maxLen = 0, center = 0;
+        for (int i = 0; i < (int)t.size(); i++) {
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                center = i;
+            }
+        }
+
+        int start = (center - maxLen + 1) / 2;
+        return s.substr(start, maxLen - 1);
     }
 };
