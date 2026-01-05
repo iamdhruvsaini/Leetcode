1a3af2e75a754b3744a72f287c0393216c8815dd
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-06 01:05:17 +0530

    Time: 564 ms (12.91%), Space: 296.7 MB (5.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int f(vector<int>& nums,int index,int prev,vector<vector<int>>&dp){
-        if(index==nums.size())return 0;
-        if(dp[index][prev+1]!=-1)return dp[index][prev+1];
+    int f(vector<int>& nums,int i,int prev,vector<vector<int>>&dp){
+        int n=nums.size();
+        if(i==n)return 0;
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
 
-        int notTake=f(nums,index+1,prev,dp);
+        int skip=f(nums,i+1,prev,dp);
         int take=0;
-        if(prev==-1 || nums[index]>nums[prev]){
-            take=1+f(nums,index+1,index,dp);
+        if(prev==-1 || nums[i]>nums[prev]){
+            take=1+f(nums,i+1,i,dp);
         }
-
-        return dp[index][prev+1]=max(notTake,take);
-
+        return dp[i][prev+1]=max(skip,take);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
+        // we have to shift the coordinates
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
         return f(nums,0,-1,dp);
         
