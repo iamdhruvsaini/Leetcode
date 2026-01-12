a486c2073c52cf49a4fa28f05186d4233338eb25
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-12 11:56:16 +0530

    Time: 0 ms (100%), Space: 15.2 MB (21.28%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,78 +1,65 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
+
+        queue<pair<int,int>>q;
         int n=board.size();
         int m=board[0].size();
-
         vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<int,int>>q;
 
-        for(int c=0;c<m;c++){
-            if(board[0][c]=='O'){
-                q.push({0,c});
-                vis[0][c]=1;
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O'){
+                q.push({0,i});
+                vis[0][i]=1;
             }
-
-            if(board[n-1][c]=='O'){
-                q.push({n-1,c});
-                vis[n-1][c]=1;
+            if(board[n-1][i]=='O'){
+                q.push({n-1,i});
+                vis[n-1][i]=1;
             }
         }
 
-        for(int r=0;r<n;r++){
-            if(board[r][0]=='O'){
-                q.push({r,0});
-                vis[r][0]=1;
+        for(int i=1;i<n-1;i++){
+            if(board[i][0]=='O'){
+                q.push({i,0});
+                vis[i][0]=1;
             }
-            if(board[r][m-1]=='O'){
-                q.push({r,m-1});
-                vis[r][m-1]=1;
+            if(board[i][m-1]=='O'){
+                q.push({i,m-1});
+                vis[i][m-1]=1;
             }
         }
 
-        int delRow[]={-1,0,1,0};
-        int delCol[]={0,1,0,-1};
+        int delr[]={-1,0,1,0};
+        int delc[]={0,1,0,-1};
 
         while(!q.empty()){
-            auto p=q.front();
+            auto node=q.front();
             q.pop();
 
-            int r=p.first;
-            int c=p.second;
+            int r=node.first;
+            int c=node.second;
 
             for(int i=0;i<4;i++){
-                int nrow=r+delRow[i];
-                int ncol=c+delCol[i];
-
-                if(nrow>=0 && nrow<n 
-                    && ncol>=0 && ncol<m 
-                    && board[nrow][ncol]=='O' 
-                    && !vis[nrow][ncol]
-                ){
-                    // board[n]
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
-                }
+                int newr=delr[i]+r;
+                int newc=delc[i]+c;
+                if(newr >=0 && newr<n && newc>=0 && newc < m
+                    && !vis[newr][newc] && board[newr][newc]=='O'){
+                        q.push({newr,newc});
+                        vis[newr][newc]=1;
+                    }
             }
 
         }
 
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==1){
-                    board[i][j]='O';
-                }
-                else{
-                    board[i][j]='X';
-                }
-            }
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+        if(board[i][j]=='O' && !vis[i][j]){
+            board[i][j]='X';   // flip only surrounded O's
         }
+    }
+}
 
-  
-
-
-
-
+        return ;
     }
 };
