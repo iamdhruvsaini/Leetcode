2b06b7ac07ea85357b1f87fd836863b65c028195
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-12 15:10:57 +0530

    Time: 9 ms (51.38%), Space: 31.7 MB (73.16%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>&vis,vector<vector<int>>&grid){
-        vis[r][c]=1;
-        int n=grid.size();
-        int m=grid[0].size();
-        int delRow[]={-1,0,1,0};
-        int delCol[]={0,1,0,-1};
-
-        for(int i=0;i<4;i++){
-            int row=delRow[i]+r;
-            int col=delCol[i]+c;
-
-            if(row>=0 && row<n && col>=0 && col<m
-                && !vis[row][col] && grid[row][col]==1){
-                    dfs(row,col,vis,grid);
-                }
-        }
-    }
     int numEnclaves(vector<vector<int>>& grid) {
-        
         int n=grid.size();
         int m=grid[0].size();
-
+        queue<pair<int,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
-    
-
         for(int i=0;i<m;i++){
             if(grid[0][i]==1){
-               dfs(0,i,vis,grid);
+                q.push({0,i});
+                vis[0][i]=1;
             }
             if(grid[n-1][i]==1){
-               dfs(n-1,i,vis,grid);
+                q.push({n-1,i});
+                vis[n-1][i]=1;
             }
         }
 
-         for(int i=0;i<n;i++){
+        for(int i=1;i<n-1;i++){
             if(grid[i][0]==1){
-               dfs(i,0,vis,grid);
+                q.push({i,0});
+                vis[i][0]=1;
             }
             if(grid[i][m-1]==1){
-               dfs(i,m-1,vis,grid);
+                q.push({i,m-1});
+                vis[i][m-1]=1;
             }
         }
 
-        // cnt=0;
+        int delr[]={1,0,-1,0};
+        int delc[]={0,-1,0,1};
+
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            int r=node.first;
+            int c=node.second;
+
+            for(int i=0;i<4;i++){
+                int nr=r+delr[i];
+                int nc=c+delc[i];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m 
+                    && !vis[nr][nc] && grid[nr][nc]==1){
+                        q.push({nr,nc});
+                        vis[nr][nc]=1;
+                    }
+            }
+
+        }
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -52,10 +56,9 @@ public:
                 }
             }
         }
-
         return cnt;
 
 
-
+        
     }
 };
