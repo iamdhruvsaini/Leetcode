8b89c506bce354e0685545c2a540975870263522
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-17 23:34:32 +0530

    Time: 62 ms (44.12%), Space: 24.6 MB (43.99%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,56 +1,49 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        
-        priority_queue<
-            pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>,
-            greater<pair<int,pair<int,int>>>
-        >pq;
-
         int n=heights.size();
         int m=heights[0].size();
 
+        // declaring the min heap
+        priority_queue<
+        pair<int,pair<int,int>>,
+        vector< pair<int,pair<int,int>>>,
+        greater< pair<int,pair<int,int>>>
+        >pq;
+
+        // it stores the diff
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
         pq.push({0,{0,0}});
-        vector<vector<int>>mat(n,vector<int>(m,INT_MAX));
-        mat[0][0]=0;
-
-
-        int delr[]={0,1,0,-1};
-        int delc[]={1,0,-1,0};
-
+        dist[0][0]=0;
+        int delr[]={-1,0,1,0};
+        int delc[]={0,1,0,-1};
         while(!pq.empty()){
             auto node=pq.top();
             pq.pop();
             int diff=node.first;
             int r=node.second.first;
             int c=node.second.second;
-
-            if(r==n-1 && c==m-1){
-                return diff;
-            }
-
+            
+            // now traverse the neighbour nodes
             for(int i=0;i<4;i++){
-                int nr=delr[i]+r;
-                int nc=delc[i]+c;
-
-
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int d=max(abs(heights[nr][nc]-heights[r][c]),diff);
-                    if(d < mat[nr][nc]){
-                        mat[nr][nc]=d;
-                        pq.push({d,{nr,nc}});
+                int newr=delr[i]+r;
+                int newc=delc[i]+c;
+                if(newr>=0 && newc>=0 && newr<n && newc<m){
+                    int newDiff=max(diff,abs(heights[newr][newc]-heights[r][c]));
+                    if(newDiff < dist[newr][newc]){
+                        dist[newr][newc]=newDiff;
+                        pq.push({newDiff,{newr,newc}});
                     }
-                   
                 }
 
-
-
             }
+
+
+
         }
 
-        return -1;
+        return dist[n-1][m-1];
 
-
+        
     }
 };
