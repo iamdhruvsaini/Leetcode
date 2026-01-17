63887cc66079843cfbc408390cf72d863b7ca216
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-17 23:03:49 +0530

    Time: 35 ms (26.62%), Space: 23.6 MB (82.28%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,62 +1,53 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if(grid[0][0]!=0)return -1;
 
-        
         int n=grid.size();
-        int m=grid[0].size();
-        if(grid[0][0]==1){
-            return -1;
-        }
-
-        if(n*m==1)return 1;
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        vector<vector<int>>dist(n,vector<int>(n,1e9));
 
         priority_queue<
-            pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>,
-            greater<pair<int,pair<int,int>>>>pq;
-        pq.push({1,{0,0}});
-        dist[0][0]=1;
+        pair<int,pair<int,int>>,
+        vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>
+        >pq;
 
-        int delr[]={0,1,1,1,0,-1,-1,-1};
-        int delc[]={1,1,0,-1,-1,-1,0,1};
+        pq.push({0,{0,0}});
+        dist[0][0]=0;
 
-
+        
+        int delr[]={-1,-1,0,1,1,1,0,-1};
+        int delc[]={0,1,1,1,0,-1,-1,-1};
 
         while(!pq.empty()){
 
-            auto top=pq.top();
+            auto node=pq.top();
             pq.pop();
 
-            int d=top.first;
-            int r=top.second.first;
-            int c=top.second.second;
+            int cost=node.first;
+            int r=node.second.first;
+            int c=node.second.second;
+            if(r==n-1 && c==n-1)return cost+1;
 
             for(int i=0;i<8;i++){
+                int newr=r+delr[i];
+                int newc=c+delc[i];
 
-                int newr=delr[i]+r;
-                int newc=delc[i]+c;
 
-                if(newr >=0 && newr<n && newc >= 0 && newc<m
-                && grid[newr][newc]!=1
-                && d+1 < dist[newr][newc]
-                ){
-                    dist[newr][newc]=d+1;
-                    pq.push({d+1,{newr,newc}});
-                }
-
-                if(newr==n-1 && newc==n-1 && grid[newr][newc]==0){
-                    return dist[n-1][n-1];
+                if(newr>=0 && newc>=0 && newr<n && newc<n && grid[newr][newc]==0){
+                    if(cost+1 < dist[newr][newc]){
+                        dist[newr][newc]=cost+1;
+                      
+                        pq.push({cost+1,{newr,newc}});
+                    }
                 }
 
             }
 
+            
         }
 
-
         return -1;
-
         
     }
 };
