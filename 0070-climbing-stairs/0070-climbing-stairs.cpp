2ffd9bc8df16d7a87ff778a371502d16474cdbe2
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-28 23:07:23 +0530

    Time: 0 ms (100%), Space: 8.8 MB (17.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-        if(n<0)return 0;
-        if(n==0)return 1;
-        if(dp[n]!=-1)return dp[n];
-
-        int case1=solve(n-1,dp);
-        int case2=solve(n-2,dp);
-
-        return dp[n]=case1+case2;
-    }
-
-    int tabulation(int n){
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-
-        for(int i=1;i<=n;i++){
-            int case1=0;
-            if(i-1 >=0){
-                case1=dp[i-1];
-            }
-            int case2=0;
-            if(i-2>=0){
-                case2=dp[i-2];
-            }
-
-            dp[i]=case1+case2;
-
-        }
-
-
-        return dp[n];
-    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return tabulation(n);
+        vector<int>dp(n+1,0); 
+        function<int(int)>fnc=[&](int a){
+            if(a==0){
+                return 1;
+            }
+            if(a<0)return 0;
+            if(dp[a]!=0)return dp[a];
+            // 1 step
+            int case1=fnc(a-1);
+            int case2=fnc(a-2);
 
+            return dp[a]=case1+case2;
+        };
+        return fnc(n);
     }
 };
