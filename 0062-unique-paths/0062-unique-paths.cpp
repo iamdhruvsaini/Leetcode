d0e688944ced8d6c2837c5a7069168e9cae315c3
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-29 23:44:31 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,6 +12,23 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(n,m,m-1,n-1,dp);
+        // initializing the base case
+        dp[0][0]=1;
+
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(row==0 && col==0)continue;
+                int case1=0;
+                if(row>=1){
+                    case1=dp[row-1][col];
+                }
+                int case2=0;
+                if(col>=1){
+                    case2=dp[row][col-1];
+                }
+                dp[row][col]=case1+case2;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
