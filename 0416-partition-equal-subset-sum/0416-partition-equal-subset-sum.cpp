0cfe30ce3de283c805d534b9811a4a07537cc5ec
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-01-31 00:19:36 +0530

    Time: 131 ms (54.06%), Space: 110.6 MB (32.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums,int index,int target,vector<vector<int>>&dp){
-        if(target==0)return true;
-        if(index==0)return nums[index]==target;
-        if(dp[index][target]!=-1)return dp[index][target];
-    
-        bool exclude=solve(nums,index-1,target,dp);
-        bool include=false;
-
-        if(target<=nums[index]){
-            include=solve(nums,index-1,target-nums[index],dp);
+    bool solve(vector<int>& nums,int sum,int index, vector<vector<int>>&dp){
+        if(sum==0)return true;
+        if(index==0){
+            return sum==nums[index];
         }
-
-        return dp[index][target]=include||exclude;
-    
+        if(dp[index][sum]!=-1)return dp[index][sum];
+        int notTake=solve(nums,sum,index-1,dp);
+        int take=0;
+        if(nums[index]<=sum){
+            take=solve(nums,sum-nums[index],index-1,dp);
+        }
+        return dp[index][sum]=take||notTake;
     }
-
     bool canPartition(vector<int>& nums) {
+        int total_sum=accumulate(nums.begin(),nums.end(),0);
+        if(total_sum & 1)return false;
         int n=nums.size();
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum&1)return false;
-        int target=sum/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,0));
-        
-        // return solve(nums,n-1,sum/2,dp);
-
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
-        if (nums[0] <= target) dp[0][nums[0]] = true;
-
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=target;j++){
-                bool exclude=dp[i-1][j];
-                bool include=false;
-                if(nums[i]<=j){
-                    include=dp[i-1][j-nums[i]];
-                }
+        vector<vector<int>>dp(n,vector<int>(total_sum/2 + 1,-1));
+        return solve(nums,total_sum/2,n-1,dp);
     
-               dp[i][j]=include||exclude;
-            }
-        }
-
-        return dp[n-1][target];
     }
 };
