27a1c7eef0c44e704d8caf0ac7175f72d7486a63
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-19 01:12:37 +0530

    Time: 7 ms (86.54%), Space: 11.4 MB (51.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
+    /*
+        aba -> 3
+        #a#b#a# -> 7
+
+    */
     string longestPalindrome(string s) {
-        string str="#";
-
-        for(auto ch:s){
-            str+=ch;
+        string str="";
+        for(int i=0;i<s.size();i++){
             str+='#';
+            str+=s[i];
         }
-
-        int n=str.length();
-        vector<int>p(n,1);
+        str+='#';
+        int n=str.size();
         int l=0,r=0;
+        vector<int>p(n,0);
+
         for(int i=0;i<n;i++){
             if(i<r){
-                p[i]=min(r-i,p[r+l-i]);
+                p[i]=min(r-i,p[l+r-i]);
             }
-            while(i+p[i]<n && i-p[i]>=0 && str[i+p[i]]==str[i-p[i]]){
+             while(i+p[i]<n && i-p[i]>=0 && str[i+p[i]]==str[i-p[i]]){
                 p[i]++;
             }
             if(i+p[i]>r){
@@ -23,9 +28,7 @@ public:
                 r=i+p[i];
             }
         }
-        // for(int i=0;i<n;i++){
-        //     cout<<p[i]<<" ";
-        // }
+
         int maxLen = 0, center = 0;
         for (int i = 0; i < n; i++) {
             if (p[i] > maxLen) {
@@ -33,9 +36,8 @@ public:
                 center = i;
             }
         }
-
         
-        int start = (center - maxLen + 1) / 2;
+        int start = (center -(maxLen - 1)) / 2;
         return s.substr(start, maxLen - 1);
         
     }
