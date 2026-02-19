c1a8816cf6a6eff1ec41951889c7d10a2f126d55
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-19 21:03:32 +0530

    Time: 104 ms (68.88%), Space: 110.4 MB (39.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -16,9 +16,26 @@ public:
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int total=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>dp(n,vector<int>(total+1,-1));
         if(total & 1)return false;
-        return solve(nums,n-1,total/2,dp);
+        int target=total/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,0));
+
+
+        for(int i=0;i<n;i++)dp[i][0]=1;
+        if(nums[0]<=target)dp[0][nums[0]]=1;
+
+        for(int index=1;index<n;index++){
+            for(int sum=1;sum<=target;sum++){
+                int notTake=dp[index-1][sum];
+                int take=false;
+                if(sum - nums[index]>=0){
+                    take=dp[index-1][sum-nums[index]];
+                }
+                dp[index][sum]= take || notTake;
+
+            }
+        }
+        return dp[n-1][target];
 
     }
 };
