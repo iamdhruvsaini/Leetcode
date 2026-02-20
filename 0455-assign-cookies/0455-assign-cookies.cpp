a44e03320e5d752387054f57bf8db07ab7a9a6f3
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-20 23:08:20 +0530

    Time: 6 ms (76.1%), Space: 44.9 MB (11.8%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -4,14 +4,17 @@ public:
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
 
-        int l=0,r=0,ans=0;
-        while(r<s.size() && l<g.size()){
-            if(g[l]<=s[r]){
-                ans++;
+        int l=0,r=0;
+        while(l<s.size() && r<g.size()){
+            if(g[r]<=s[l]){
+                l++;
+                r++;
+            }
+            else{
                 l++;
             }
-            r++;
         }
-        return ans;
+        return r;
+        
     }
 };
