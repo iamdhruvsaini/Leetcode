4d9378532ff342b6d7e86edfeef1b12983e933e1
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-23 23:26:32 +0530

    Time: 82 ms (49%), Space: 76.3 MB (8.7%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,50 +1,21 @@
 class Solution {
 public:
+    int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+        
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        if(s1[i]==s2[j]){
+            return 1+solve(s1,s2,i-1,j-1,dp);
+        }
+        return dp[i][j] = max(solve(s1,s2,i,j-1,dp),solve(s1,s2,i-1,j,dp));
+    }
     int longestPalindromeSubseq(string s) {
-
-        string text1=s;
-        reverse(s.begin(),s.end());
-        string text2=s;
-        int n=text1.size();
-        int m=text2.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-
-        function<int(int,int)>fnc=[&](int index1,int index2){
-            if(index1 == 0 || index2 == 0)return 0;
-            
-            if(dp[index1][index2]!=-1)return dp[index1][index2];
-
-            if(text1[index1-1]==text2[index2-1]){
-                return dp[index1][index2]=1+fnc(index1-1,index2-1);
-            }
-
-
-            return dp[index1][index2]=max(fnc(index1-1,index2),fnc(index1,index2-1));
-        
-        };
-
-
-        auto tabulation=[&](){
-            for(int i=0;i<=n;i++)dp[i][0]=0;
-            for(int i=0;i<=m;i++)dp[0][i]=0;
-
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=m;j++){
-                    if(text1[i-1]==text2[j-1]){
-                        dp[i][j]=1+dp[i-1][j-1];
-                    }
-                    else{
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    }
-                }
-            }
-
-            return dp[n][m];
-        };
-
-        return tabulation();
-
-        
+        string s1=s;
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve(s1,s2,n-1,n-1,dp);
     }
 };
