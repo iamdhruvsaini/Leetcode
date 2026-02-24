3fd64b801ed2508539c0e3722044df967d49ed70
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-24 23:58:38 +0530

    Time: 35 ms (50.26%), Space: 31.6 MB (17.02%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int minInsertions(string s) {
-
-        string t1=s;
-        reverse(s.begin(),s.end());
-        string t2=s;
-        int n=t1.length();
-        int m=t2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-
-        auto fnc=[&](){
-            for(int i=0;i<=n;i++)dp[i][0]=0;
-            for(int j=0;j<=m;j++)dp[0][j]=0;
-
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=m;j++){
-                    if(t1[i-1]==t2[j-1]){
-                        dp[i][j]=1+dp[i-1][j-1];
-                    }
-
-                    else{
-                        dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                    }
+    int f( string s1, string s2, vector<vector<int>>&dp){
+        int n=s1.size();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
                 }
             }
-            return dp[n][m];
-        };
-
-        return n-fnc();
-        
+        }
+        return dp[n][n];
+    }
+    int minInsertions(string s) {
+       string s1=s;
+       reverse(s1.begin(),s1.end());
+       int n=s.size();
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+       int lps=f(s1,s,dp); 
+       return n-lps;
     }
 };
