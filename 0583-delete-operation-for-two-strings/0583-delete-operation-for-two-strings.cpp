f34eede2301794557b62329017cae6ad688e7589
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-02-25 22:46:05 +0530

    Time: 13 ms (47.03%), Space: 16.7 MB (57.03%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        // delete = n-lcs
-        // add = m-lcs
-
-        int n=word1.length();
-        int m=word2.length();
-
+        int n=word1.size(),m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        auto fnc=[&](){
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=m;j++){
-                    if(word1[i-1]==word2[j-1]){
-                        dp[i][j]=1+dp[i-1][j-1];
-                    }
-                    else{
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            return dp[n][m];
-        };
+        }
+        int common=dp[n][m];
+        return n+m - 2*common;
         
-        return n+m-2*fnc();
     }
 };
