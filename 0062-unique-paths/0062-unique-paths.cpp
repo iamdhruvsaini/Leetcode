ef95cb53c93dc4870b7ce48e6ee29071be4a7775
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-03 12:48:48 +0530

    Time: 0 ms (100%), Space: 9.4 MB (51.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,27 @@
 class Solution {
-public: 
-    int solve(int n,int m ,int row,int col,vector<vector<int>>&dp){
-        if(row==0 && col==0)return 1;
-        if(row<0 || col<0)return 0;
-        if(dp[row][col]!=-1)return dp[row][col];
-        // move top
-        int case1=solve(n,m,row-1,col,dp);
-        int case2=solve(n,m,row,col-1,dp);
+public:
+    int f(int n,int m,int r,int c, vector<vector<int>>&dp){
+        if(r==0 && c==0)return 1;
 
-        return dp[row][col]=case1+case2;
+        if(r<0 || c<0 || r>=n || c>=m)return 0;
+        if(dp[r][c]!=-1)return dp[r][c];
+
+
+        // robot can move up or left only
+        // up case
+        int case1=f(n,m,r-1,c,dp);
+        // down case
+        int case2=f(n,m,r,c-1,dp);
+
+        return dp[r][c]=case1+case2;
+
+
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        // initializing the base case
-        dp[0][0]=1;
 
-        for(int row=0;row<m;row++){
-            for(int col=0;col<n;col++){
-                if(row==0 && col==0)continue;
-                int case1=0;
-                if(row>=1){
-                    case1=dp[row-1][col];
-                }
-                int case2=0;
-                if(col>=1){
-                    case2=dp[row][col-1];
-                }
-                dp[row][col]=case1+case2;
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        
+        return f(m,n,m-1,n-1,dp);
+        
     }
 };
