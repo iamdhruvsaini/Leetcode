d95f6b9054949e5ac79197331df5895737703a74
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-03 13:05:34 +0530

    Time: 0 ms (100%), Space: 12 MB (6.43%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int r,int c,vector<vector<int>>&dp){
-        if(r<0 || c<0)return 0;
-        if(grid[r][c]==1)return 0;
+    int f(vector<vector<int>>& mat,int r,int c,  vector<vector<int>>&dp){
+        int n=mat.size(),m=mat[0].size();
+        if(r<0 || c<0 || r>=n || c>=m)return 0;
+        // if there is a stone in path
+        if(mat[r][c]==1)return 0;
+
         if(r==0 && c==0)return 1;
         if(dp[r][c]!=-1)return dp[r][c];
-        int case1=solve(grid,r,c-1,dp);
-        int case2=solve(grid,r-1,c,dp);
+
+        // upward move
+        int case1=f(mat,r-1,c,dp);
+        // left move
+        int case2=f(mat,r,c-1,dp);
+
         return dp[r][c]=case1+case2;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-
-    if(grid[0][0] == 1) return 0;
-
-    vector<vector<int>> dp(n, vector<int>(m, 0));
-    dp[0][0] = 1;
-
-    for(int r = 0; r < n; r++) {
-        for(int c = 0; c < m; c++) {
-            if(grid[r][c] == 1) {
-                dp[r][c] = 0;
-                continue;
-            }
-            if(r == 0 && c == 0) continue;
-
-            int case1 = (c >= 1) ? dp[r][c-1] : 0;
-            int case2 = (r >= 1) ? dp[r-1][c] : 0;
-
-            dp[r][c] = case1 + case2;
-        }
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(mat,n-1,m-1,dp);
     }
-    return dp[n-1][m-1];
-}
-
 };
