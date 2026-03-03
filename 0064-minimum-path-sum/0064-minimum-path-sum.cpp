93fd474f894199f176a83d86d35b3d325f9c2ebd
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-03 13:45:48 +0530

    Time: 4 ms (14.84%), Space: 16.4 MB (16.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -17,8 +17,28 @@ public:
         int m=mat[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        return f(mat,n-1,m-1,dp);
-        
+        dp[0][0]=mat[0][0];
+
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+    
+                if(r==0 && c==0)continue;
+                int case1=1e9;
+                if(r-1 >=0){
+                    case1=mat[r][c]+f(mat,r-1,c,dp);
+                }
+                // move left
+                int case2=1e9;
+                if(c-1>=0){
+                    case2=mat[r][c]+f(mat,r,c-1,dp);
+                }
+
+                dp[r][c] = min(case1,case2);
+
+            }
+        }
+
+        return dp[n-1][m-1];
         
     }
 };
