968e42100db1458dcaf8a8197aec3229b5cd0800
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-11 22:30:55 +0530

    Time: 11 ms (63.9%), Space: 12.5 MB (94.94%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -4,31 +4,34 @@ public:
         int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<int>dp(n,1);
-        vector<int>hash(n,0);
-        vector<int>ans;
+        vector<int>hash(n);
         for(int i=0;i<n;i++)hash[i]=i;
 
-        int maxi=-1,index=0;
-
+        int maxi=0,max_index=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+            for(int j=i-1;j>=0;j--){
                 if(nums[i]%nums[j]==0 && dp[j]+1 > dp[i]){
                     dp[i]=dp[j]+1;
                     hash[i]=j;
                 }
             }
-            if(dp[i]>maxi){
+            if(maxi < dp[i]){
                 maxi=dp[i];
-                index=i;
+                max_index=i;
             }
         }
+        int index=max_index;
+        vector<int>ans;
         ans.push_back(nums[index]);
-        while(index!=hash[index]){
+        while(hash[index]!=index){
             index=hash[index];
             ans.push_back(nums[index]);
         }
-
         reverse(ans.begin(),ans.end());
         return ans;
+
+
+
+        
     }
 };
