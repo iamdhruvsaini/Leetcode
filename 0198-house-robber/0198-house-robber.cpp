0e09d524b7497819b640e2481ad6980070f21c88
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-13 23:10:47 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int index){
 
-        if(index==0)return nums[0];
-        if(index<0)return 0;
-        // skip current
-        int case1=solve(nums,index-1);
-       
-        int case2=nums[index]+solve(nums,index-2);
-
-        return max(case1,case2);
+int f(vector<int>&arr,int i,int rob,vector<vector<int>>&dp){
+    if(i==0){
+        if(rob)return arr[i];
+        return 0;
     }
+    if(dp[i][rob+1]!=-1)return dp[i][rob+1];
+    // not rob
+    int case1=f(arr,i-1,1,dp);
+    int case2=0;
+    if(rob==-1 || rob==1){
+        // can rob it
+        case2=arr[i]+f(arr,i-1,0,dp);
+    }
+    return dp[i][rob+1] = max(case1, case2);
+}
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n+1,0);
-        dp[1]=nums[0];
-
-        for(int i=2;i<=n;i++){
-            int case1=dp[i-1];
-       
-            int case2=nums[i-1]+dp[i-2];
-            dp[i]=max(case1,case2);
-
-        }
-
-        return dp[n];
-
-
-        // return solve(nums,n-1);
+    int n=nums.size();
+    vector<vector<int>>dp(n,vector<int>(n+1,-1));
+     return f(nums,n-1,-1,dp);
         
     }
 };
+
