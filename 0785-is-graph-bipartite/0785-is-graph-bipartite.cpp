6dfbf5ac961f29e6efe2702de7dce9bb126f6697
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-03-14 22:40:22 +0530

    Time: 0 ms (100%), Space: 17.4 MB (77.17%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool dfs(int node,int clr,vector<vector<int>>& graph,vector<int> &vis){
-        vis[node]=clr;
-
-        for(auto it: graph[node]){
-            if(vis[it]==-1){
-               if (!dfs(it, !clr, graph, vis)) {
-                    return false;
-                }
+    bool dfs(vector<vector<int>>& graph, vector<int>& vis, int node){
+        for(auto it : graph[node]){
+            if(vis[it] == -1){
+                vis[it] = !vis[node];
+                if(!dfs(graph, vis, it)) return false;
             }
-            else if(vis[it]==vis[node]){
-                return false;
-
+            else{
+                if(vis[it] == vis[node]) return false;
             }
         }
-
         return true;
     }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, -1);  // -1 = uncolored, 0 and 1 are two colors
+        vector<int> vis(n, -1);
 
-        for(int i = 0; i < n; i++) {
-            if(vis[i] == -1 && dfs(i,1,graph,vis)==false) {
-                return false;
-                
+        for(int i = 0; i < n; i++){
+            if(vis[i] == -1){
+                vis[i] = 0;   // assign initial color
+                if(!dfs(graph, vis, i)) return false;
             }
         }
+
         return true;
     }
 };
