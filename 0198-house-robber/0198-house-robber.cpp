794ce2e3e31504c6e268ce49449c504a26bb871e
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-05-14 18:28:24 +0530

    Time: 1 ms (9.01%), Space: 10.6 MB (59.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-
-int f(vector<int>&arr,int i,int rob,vector<vector<int>>&dp){
-    if(i==0){
-        if(rob)return arr[i];
-        return 0;
+    int f(vector<int>&nums,int i, vector<int>&dp){
+        if(i==0)return nums[i];
+        if(i<0)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int notTake=f(nums,i-1,dp);   
+        int take=nums[i]+f(nums,i-2,dp);
+     
+        return dp[i]=max(take,notTake);
     }
-    if(dp[i][rob+1]!=-1)return dp[i][rob+1];
-    // not rob
-    int case1=f(arr,i-1,1,dp);
-    int case2=0;
-    if(rob==-1 || rob==1){
-        // can rob it
-        case2=arr[i]+f(arr,i-1,0,dp);
-    }
-    return dp[i][rob+1] = max(case1, case2);
-}
     int rob(vector<int>& nums) {
-    int n=nums.size();
-    vector<vector<int>>dp(n,vector<int>(n+1,-1));
-     return f(nums,n-1,-1,dp);
-        
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return f(nums,n-1,dp);
     }
 };
-
