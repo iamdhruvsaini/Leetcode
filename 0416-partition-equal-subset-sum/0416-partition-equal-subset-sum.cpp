462e866e5f9a77bada75ac11afd09448af2f98be
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-05-15 14:27:20 +0530

    Time: 127 ms (54.96%), Space: 110.6 MB (30.86%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums,int index,int sum, vector<vector<int>>&dp){
-        if(sum == 0)return true;
-        if(index==0)return sum==nums[index];
-        if(dp[index][sum]!=-1)return dp[index][sum];
+    int f(vector<int>& nums,int i,int t,vector<vector<int>>&dp){
+        if(t==0)return 1;
+        if(i==0)return nums[i]==t;
+        if(dp[i][t]!=-1)return dp[i][t];
 
-        // nottake
-        int notTake=solve(nums,index-1,sum,dp);
-        int take=false;
-        if(sum - nums[index]>=0){
-            take=solve(nums,index-1,sum-nums[index],dp);
+        int case1=f(nums,i-1,t,dp);
+        int case2=0;
+        if(t-nums[i]>=0){
+            case2=f(nums,i-1,t-nums[i],dp);
         }
-        return dp[index][sum]= take || notTake;
+        return dp[i][t]=case1||case2;
     }
     bool canPartition(vector<int>& nums) {
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if(sum&1)return 0;
         int n=nums.size();
-        int total=accumulate(nums.begin(),nums.end(),0);
-        if(total & 1)return false;
-        int target=total/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,0));
+        vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
 
-
-        for(int i=0;i<n;i++)dp[i][0]=1;
-        if(nums[0]<=target)dp[0][nums[0]]=1;
-
-        for(int index=1;index<n;index++){
-            for(int sum=1;sum<=target;sum++){
-                int notTake=dp[index-1][sum];
-                int take=false;
-                if(sum - nums[index]>=0){
-                    take=dp[index-1][sum-nums[index]];
-                }
-                dp[index][sum]= take || notTake;
-
-            }
-        }
-        return dp[n-1][target];
-
+        return f(nums,n-1,sum/2,dp);
     }
 };
