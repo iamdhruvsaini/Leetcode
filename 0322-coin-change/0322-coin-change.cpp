b9d786e1869d367565835e1465804ca14bfa3547
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-05-15 17:49:40 +0530

    Time: 50 ms (23.83%), Space: 49.3 MB (26.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
-    int f(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
-
+    int f(vector<int>&coins,int i,int amt, vector<vector<int>>&dp){
+        if(amt==0)return 0;
+        // coins made by 0th coins
         if(i==0){
-            if(amount%coins[i]==0)return amount/coins[i];
+            if(amt%coins[i]==0)return amt/coins[i];
             else return 1e9;
         }
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        // not take current coin
-        int notTake=f(coins,amount,i-1,dp);
+        if(dp[i][amt]!=-1)return dp[i][amt];
 
-        // take the current coin
+        int notTake=0+f(coins,i-1,amt,dp);
+        // take
         int take=1e9;
-        if(amount-coins[i]>=0){
-            // add 1 for considering th coin
-            take=1+f(coins,amount-coins[i],i,dp);
+
+        if(amt-coins[i]>=0){
+            // as we can repeat the coins na
+            take=1+f(coins,i,amt-coins[i],dp);
         }
 
-        return dp[i][amount] = min(take,notTake);
+        return dp[i][amt]=min(take,notTake);
 
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int>dp1(amount+1,-1),dp2(amount+1,-1);
-        // int ans=f(coins,amount,n-1,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
 
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0)dp1[i]=i/coins[0];
-            else dp1[i]=1e9;
-        }
-
-        for(int i=1;i<n;i++){
-            for(int amt=0;amt<=amount;amt++){
-                int notTake=dp1[amt];
-                 // take the current coin
-                 int take=1e9;
-                 if(amt-coins[i]>=0){
-                     // add 1 for considering th coin
-                     take=1+dp2[amt-coins[i]];
-                 }
-                 dp2[amt] = min(take,notTake);
-
-            }
-            dp1=dp2;
-        }
-        if(dp1[amount]!=1e9)return dp1[amount];
-        return -1;
+        int ans=f(coins,n-1,amount,dp);
+        return ans==1e9?-1:ans;
     }
 };
