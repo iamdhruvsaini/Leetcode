63af18957fcc5541e2a9aedf583e85cea15f5043
Dhruv Saini <133573695+iamdhruvsaini@users.noreply.github.com>
2026-05-18 08:53:41 +0530

    Time: 57 ms (41.4%), Space: 54.7 MB (13.86%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,72 +1,65 @@
 class Solution {
 public:
-    bool solve(string s,string p,int i,int j,vector<vector<int>>&dp){
-        
-        if(i==0 && j==0)return true;
-        if(i==0){
-            for(;j>=1;j--){
-                if(p[j-1]!='*')return false;
-            }
-            return true;
-        }
-        if(j==0){
-            return false;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
+    int f(string s, string p, int i, int j, vector<vector<int>>& dp) {
+        if(i == 0 && j == 0) return 1;
 
-        if(s[i-1]==p[j-1] || p[j-1]=='?'){
-            return dp[i][j] = solve(s,p,i-1,j-1,dp);
+        // pattern exhausted
+        if(j == 0) return 0;
+
+        // string exhausted
+        if(i == 0) {
+            for(int k = 1; k <= j; k++) {
+                if(p[k-1] != '*') return 0;
+            }
+            return 1;
         }
-        else if(p[j-1]=='*'){
-            // two cases possible
-            return dp[i][j] = solve(s,p,i,j-1,dp) || solve(s,p,i-1,j,dp);
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i-1] == p[j-1] || p[j-1] == '?') {
+            return dp[i][j] = f(s, p, i-1, j-1, dp);
         }
-        return false;
-    } 
+
+        else if(p[j-1] == '*') {
+            int case1 = f(s, p, i-1, j, dp);   // match one/more chars
+            int case2 = f(s, p, i, j-1, dp);   // match empty
+            return dp[i][j] = case1 || case2;
+        }
+
+        return dp[i][j] = 0;
+    }
+
     bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        auto fnc=[&](){
-            // base case initialized
-            dp[0][0]=true;
-            for(int i=1;i<=n;i++)dp[i][0]=0;
-            
+        int n = s.size(), m = p.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        for(int i=0;i<=n;i++)dp[i][0]=0;
+        for(int j=0;j<=m;j++){
+            for(int k = 1; k <= j; k++) {
+                if(p[k-1] != '*') dp[0][j] = 0;
+            }
+            if(dp[0][j]!=0)dp[0][j]=1;
+        }
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                bool flag=true;
-                for(int jj=j;jj>=1;jj--){
-                    if(p[jj-1]!='*'){
-                        dp[0][j]= false;
-                        flag=false;
-                        break;
-                    };
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
                 }
-                if(flag){
-                    dp[0][j]=true;
+
+                else if(p[j-1] == '*') {
+                    int case1 = dp[i-1][j];   // match one/more chars
+                    int case2 = dp[i][j-1];   // match empty
+                    dp[i][j] = case1 || case2;
                 }
+
+                else dp[i][j] = 0;
+
             }
+        }
+        return dp[n][m];
 
+        // return f(s, p, n, m, dp);
 
-            for(int i=1;i<=n;i++){
-                for(int j=1;j<=m;j++){
-                    if(s[i-1]==p[j-1] || p[j-1]=='?'){
-                        dp[i][j] = dp[i-1][j-1];
-                    }
-                    else if(p[j-1]=='*'){
-                        // two cases possible
-                        dp[i][j] = dp[i][j-1] || dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j]=false;
-                    }                   
-
-                }
-            }
-
-            return dp[n][m];
-
-        };
-
-        return fnc();
     }
 };
